1c6e3b250b37a421fb479e41438bfb1e0e510eba
VenkateshKalisetty <venky12034@gmail.com>
2018-08-20 15:03:37 +0530

    in memory and buslist service add

--- src/app/busses-list/busses-list.component.ts
+++ src/app/busses-list/busses-list.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { BusList } from '../shared/iBusList';
+
+import { BussesListService } from '../shared/busses-list.service';
 
 @Component({
   selector: 'app-busses-list',
@@ -6,8 +9,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./busses-list.component.css']
 })
 export class BussesListComponent implements OnInit {
+  busList: BusList[];
+  errMsg: string;
 
-  constructor() { }
+  constructor(private bussesListService: BussesListService) {
+    this.bussesListService.getBusList().subscribe(data=>{
+      this.busList = data;
+    }, error => {this.errMsg = error});
+  }
 
   ngOnInit() {
   }
--- src/app/shared/busses-list.service.ts
+++ src/app/shared/busses-list.service.ts
@@ -1,9 +1,34 @@
 import { Injectable } from '@angular/core';
+import { BusList } from './iBusList';
+
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Observable, throwError} from 'rxjs';
+import { catchError, tap } from 'rxjs/operators';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class BussesListService {
 
-  constructor() { }
+  busListApi = "api/bussesList";
+  constructor(private http: HttpClient) { }
+
+  getBusList(): Observable<BusList[]> {
+    return this.http.get<BusList[]>(this.busListApi).pipe(
+      //tap(data => console.log(JSON.stringify(data))),
+      catchError(this.handleError)
+    );
+  }
+
+  private handleError(err: HttpErrorResponse){
+    let errorMessage = '';
+        if (err.error instanceof ErrorEvent) {
+            errorMessage = `An error occurred: ${err.error.message}`;
+        } else {
+            errorMessage = `Server returned code: ${err.status}, error message is: ${err.message}`;
+        }
+        console.error(errorMessage);
+        return throwError(errorMessage);
+  }
 }
