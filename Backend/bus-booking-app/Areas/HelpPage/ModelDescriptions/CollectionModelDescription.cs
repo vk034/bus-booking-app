df4c7ee4379e4f77d2ee6fcfc25462c67f7c1c7d
VenkateshKalisetty <venky12034@gmail.com>
2018-08-27 18:13:14 +0530

    frontend and backend added

---
+++ Backend/bus-booking-app/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace bus_booking_app.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ Backend/bus-booking-app/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace bus_booking_app.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
